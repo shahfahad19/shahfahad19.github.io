8a20e9008b3570b6943e0e7af7dbf58766e89be7
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2021-04-01 18:15:56 +0500

    Create index.php

---
+++ express/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Express Newspaper</title>
+  <link rel="icon" href="https://i.postimg.cc/dV4GF2xD/express.jpg">
+</head>
+<body style="margin:0px;" >
+  <div id="paper" style="width:100%;"></div
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Front_Page_1.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Baqia_PageC005_5.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Sports_PageC008_8.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Metropolitan_PageC002_2.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-NAT_INT_PageC003_3.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-City_PageC004_4.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Editorial_PageC006_6.jpg"><br>
+    <img src="https://www.express.com.pk/images/NP_PEW/<?php echo date("Ymd")?>/<?php echo date("Ymd")?>-NP_PEW-Classified_PageC007_7.jpg"><br>
+    </body>
+</html>

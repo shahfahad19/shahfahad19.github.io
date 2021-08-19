d216295aeb475d22bd6e0ab4db1a0617e1e6b322
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2021-08-19 21:36:35 +0500

    Update script.js

--- pknews/script.js
+++ pknews/script.js
@@ -2,6 +2,31 @@ var back=0, menu, cities, res, dl=localStorage.getItem("dlmode");
 $( document ).ready(function() {
 getList();
 $("#back").css("display", "none");
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
 });
 function getList() {
    if (dl==undefined) {
@@ -384,11 +409,4 @@ if (dl=="dark") {
    $("img").css("filter", "invert(0%)");
   } 
 }
-let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});

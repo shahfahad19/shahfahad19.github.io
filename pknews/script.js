3ee511ee86a1961b22721221e452f4ffa883da0d
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2021-08-19 20:38:41 +0500

    Update script.js

--- pknews/script.js
+++ pknews/script.js
@@ -384,3 +384,11 @@ if (dl=="dark") {
    $("img").css("filter", "invert(0%)");
   } 
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});

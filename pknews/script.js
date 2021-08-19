95b2091a68e97a311c86fd1c5ff552f4854aa81b
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2021-08-19 19:44:04 +0500

    Update script.js

--- pknews/script.js
+++ pknews/script.js
@@ -388,7 +388,7 @@ if (dl=="dark") {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("/service.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });

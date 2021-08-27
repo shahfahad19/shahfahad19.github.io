944c27531665d5ebf468215e94a7c07dd05f4485
shahfahad19 <63948421+shahfahad19@users.noreply.github.com>
2021-08-27 11:38:13 +0500

    Create serviceWorker.js

---
+++ newspapers/serviceWorker.js
@@ -0,0 +1,25 @@
+const pakNewspapers = "pak-newspapers";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/src/urdufont.ttf",
+  "/src/jquery.min.js"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(pakNewspapers).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
